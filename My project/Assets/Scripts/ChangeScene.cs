592b4d2cca1349da01c983ddda1a4aa1daecfbd6
drakeus0 <drakbin115@gmail.com>
2025-11-09 21:19:01 -0600

    Added stuff

---
+++ My project/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,38 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] string scene;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        Debug.Log(distance);
+
+        if (distance < 2)
+        {
+            text.enabled = true;
+        }
+        else
+        {
+            text.enabled = false;
+        }
+        if (distance < 2 && Keyboard.current.eKey.isPressed)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        }
+    }
+}
