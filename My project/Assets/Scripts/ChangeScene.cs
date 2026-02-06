a5f4dd65297cf64eeb54659b751ed7cb66593e33
drakeus0 <drakbin115@gmail.com>
2026-02-06 15:54:06 -0600

    sdf

--- My project/Assets/Scripts/ChangeScene.cs
+++ My project/Assets/Scripts/ChangeScene.cs
@@ -37,7 +37,6 @@ public class ChangeScene : MonoBehaviour
     void Update()
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
         if (distance < triggerDistance)
         {
             if (!isVisible)
